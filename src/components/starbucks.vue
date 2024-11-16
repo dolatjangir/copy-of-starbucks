6e42ee867f1cc2374c3e3d737462b712e130fddf
dolatjangir <dolatjangir778@gmail.com>
2024-11-17 02:25:33 +0530

    first commit

--- src/components/starbucks.vue
+++ src/components/starbucks.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class=" flex items-center justify-between px-4 sm:w-screen border 2xl:py-8 2xl:px-8 py-4">
+  <div class=" flex items-center justify-between px-4 sm:w-[screen] border 2xl:py-8 2xl:px-8 py-4">
    <div class="left_header flex flex-row items-center 2xl:space-x-8 2xl:font-bold">
     <img class="sm:w-[30.9px] 2xl:w-[49.3px] xl:w-[49.3px] lg:w-[38.7px]" src="https://www.starbucks.in/assets/icon/logo.png"/>
     <button class="menu-button sm:invisible 2xl:visible">MENU</button>
     <button class="rewards-button border-b-4 border-green-950 sm:invisible 2xl:visible">REWARD</button>
     <button class="sm:invisible 2xl:visible">GIFT CARDS</button>
    </div>
-   <div class="hamburger 2xl:hidden"><i class="fa-solid fa-bars"></i></div>
+   <div class="hamburger 2xl:hidden text-xl"><i class="fa-solid fa-bars"></i></div>
    <div class="right  sm:hidden 2xl:block 2xl:font-bold flex 2xl:space-x-4">
     <button class="2xl:pr-10"><i class="fa-solid fa-location-dot"></i> Find Store</button>
     <button class="sign-in border border-neutral-400 rounded-3xl px-4 py-2">Sign In</button>
@@ -14,9 +14,77 @@
                     </div> 
   </div>
   <!-- header-2 -->
-   <div>
-    <h1 tabindex="-1">Starbucks® Rewards</h1>
+   <div class="bg-[#1e3932] text-[#f1f0f0] font-bold xl:pl-[99px] xl:pr-[24px] uppercase justify-between items-center h-[50px] text-s leading-5 flex sm:px-4">
+    <h1 tabindex="-1" class="w-fit ">Starbucks® Rewards</h1>
+    <button class="sign-in border border-neutral-200 rounded-3xl px-4 py-1 xl:hidden text-center flex-shrink-0">Join in the app</button>
    </div>
+   <!-- page-3 -->
+   <div class="pb-9"></div>
+   <!-- part-4 -->
+    <div class="py-8 px-[16px]">
+      <div class="text-center">
+        <h1 class="sm:text-lg sm:font-semibold xl:font-bold xl:text-3xl">Getting started is easy</h1>
+        <p class="pt-3">Earn Stars and get rewarded in a few easy steps.</p>
+      </div>
+      <div class="xl:flex xl:justify-center  xl:mx-[38.4px] xl:pt-[48px] ">
+      <!-- page-4-part-1 -->
+      <div class="flex  sm:justify-between sm:pt-12 sm:visible xl:hidden">
+        <div class="flex-shrink-0 text-center sm:pr-[12px]">
+          <img class="sm:w-12 sm:h-12 " src="https://www.starbucks.com/weblx/images/rewards/getting-started/getting-started-1@2x.jpg"/>
+        </div>
+        <div class="sm:pb-8">
+          <h3 class="text-lg sm:pb-1 font-semibold" tabindex="-1">Download the Starbucks® app</h3>
+          <p class="sm:pt-3 text-[#000000DE] text-wrap xl:text-balance">
+            <link class="text-[#006241]" href="https://starbucks.app.link/VLa2I3inh9">Join in the app</link>
+           <span > to get access to the full range of Starbucks® Rewards benefits. You can also </span>
+            <link class="text-[#006241]" href="/account/create">join online</link>
+          </p>
+        </div>
+      </div>
+      <!-- page-4-part-1-page-xl-2 -->
+        <div class="flex flex-col w-[248.5px] h-[252px] items-center space-y-[28px] sm:hidden xl:block pl-[24px]">
+        <div class="flex-shrink-0 text-center sm:pr-[12px]">
+          <img class="sm:w-12 sm:h-12 " src="https://www.starbucks.com/weblx/images/rewards/getting-started/getting-started-1@2x.jpg"/>
+        </div>
+        <div class="sm:pb-8">
+          <h3 class="text-lg sm:pb-1 font-semibold" tabindex="-1">Create an account</h3>
+          <p class="sm:pt-3 text-[#000000DE] text-wrap xl:text-balance">
+           <a class="color-greenStarbucks" href="/account/create">join now</a>
+           <span >. You can also  </span>
+           <a class="color-greenStarbucks" href="https://starbucks.app.link/VLa2I3inh9" style="text-transform: lowercase;">Join in the app</a>
+         <span > to get access to the full range of Starbucks® Rewards benefits.</span>
+          </p>
+        </div>
+      </div>
+      <!-- page-4-part-2 -->
+      <div class="flex  xl:flex-col xl:w-[248.5px] xl:h-[252px] justify-between  xl:space-y-[28px] xl:items-center xl:pl-[24px] ">
+        <div class="flex-shrink-0 text-center sm:pr-[12px]">
+          <img class="sm:w-12 sm:h-12 " src="https://www.starbucks.com/weblx/images/rewards/getting-started/getting-started-2@2x.jpg"/>
+        </div>
+        <div class="sm:pb-8">
+          <h3 class="text-lg sm:pb-1 font-semibold" tabindex="-1">Order and pay how you’d like</h3>
+          <p class="sm:pt-3 text-[#000000DE] text-wrap">
+           
+           <span >Use cash, credit/debit card or save some time and pay right through the app. You’ll collect Stars all ways. </span>
+           <a class="color-greenStarbucks" href="#waystopay">Learn how</a>
+          </p>
+        </div>
+      </div>
+       <!-- page-4-part-3 -->
+       <div class="flex justify-between xl:w-[248.5px] xl:h-[252px] xl:flex-col  xl:space-y-[28px] xl:items-center xl:pl-[24px] ">
+        <div class="flex-shrink-0 text-center sm:pr-[12px]">
+          <img class="sm:w-12 sm:h-12 " src="https://www.starbucks.com/weblx/images/rewards/getting-started/getting-started-3@2x.jpg"/>
+        </div>
+        <div class="sm:pb-8">
+          <h3 class="text-lg sm:pb-1 font-semibold  " tabindex="-1">Earn Stars, get Rewards</h3>
+          <p class="sm:pt-3 text-[#000000DE] text-wrap">
+           <span > As you earn Stars, you can redeem them for Rewards—like free food, drinks, and more. Start redeeming with as little as 25 Stars! </span>
+          </p>
+        </div>
+      </div>
+      </div>
+        <!--  -->
+    </div>
 </template>
 <script>
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,23 +4,25 @@ export default {
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",],
   theme: {
-    screens: {
-      'sm': '320px',
-      // => @media (min-width: 640px) { ... }
-
-      'md': '375px',
-      // => @media (min-width: 768px) { ... }
-
-      'lg': '425px',
-      // => @media (min-width: 1024px) { ... }
-
-      'xl': '768px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1024px',
-      // => @media (min-width: 1536px) { ... }
-  },
-    extend: {},
+   
+    extend: {
+      screens: {
+        'sm': '320px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '375px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '425px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '768px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1024px',
+        // => @media (min-width: 1536px) { ... }
+    },
+    },
   },
   plugins: [],
 }
